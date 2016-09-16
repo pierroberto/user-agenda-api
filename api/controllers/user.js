5c4eab06fa5ccd6a531dd5d09be0899d0a87b5c2
John Benavides <john.benavides@digitaldimension.es>
2016-09-16 12:54:22 +0200

    initial commit

---
+++ api/controllers/user.js
@@ -0,0 +1,36 @@
+var Repository = require('../../repository/repository');
+var repo = new Repository();
+var User = repo.models.User;
+
+function getAllUsers(req, res) {
+  User.findAll({})
+    .then(function(users) {
+      res.status(200).json(users);
+    })
+    .catch(function(err) {
+      res.status(500).json({
+        message: err.message,
+      });
+    })
+}
+
+function getUser(req, res) {
+  User.findOne({
+    where: {
+      id: req.swagger.params.id.value,
+    }
+  })
+    .then(function(users) {
+      res.status(200).json(users);
+    })
+    .catch(function(err) {
+      res.status(500).json({
+        message: err.message,
+      });
+    })
+}
+
+module.exports = {
+  getAllUsers: getAllUsers,
+  getUser: getUser,
+};
