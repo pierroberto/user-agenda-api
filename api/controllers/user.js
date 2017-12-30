a34af44ba883799971172f52af218cbf8a197656
Pier Roberto Lucisano <pierroberto.lucisano@gmail.com>
2017-12-30 12:01:24 +0100

    Feat: add DEL request

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -1,4 +1,4 @@
-var Repository = require('../../repository/repository');
+var Repository = require("../../repository/repository");
 var repo = new Repository();
 var User = repo.models.User;
 
@@ -9,15 +9,15 @@ function getAllUsers(req, res) {
     })
     .catch(function(err) {
       res.status(500).json({
-        message: err.message,
+        message: err.message
       });
-    })
+    });
 }
 
-function getUser(req, res) {
-  User.findOne({
+function delUser(req, res) {
+  User.destroy({
     where: {
-      id: req.swagger.params.id.value,
+      id: req.swagger.params.id.value
     }
   })
     .then(function(users) {
@@ -25,12 +25,29 @@ function getUser(req, res) {
     })
     .catch(function(err) {
       res.status(500).json({
-        message: err.message,
+        message: err.message
       });
+    });
+}
+
+function getUser(req, res) {
+  User.findOne({
+    where: {
+      id: req.swagger.params.id.value
+    }
+  })
+    .then(function(users) {
+      res.status(200).json(users);
     })
+    .catch(function(err) {
+      res.status(500).json({
+        message: err.message
+      });
+    });
 }
 
 module.exports = {
   getAllUsers: getAllUsers,
   getUser: getUser,
+  delUser: delUser
 };
